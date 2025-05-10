8fae962f2b77e5a1497579a890af4e58f56cd1b8
Farrux Temurov <tfarrux70@gmail.com>
2025-05-10 22:51:59 +0500

    Updated stocks.route.js

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -5,7 +5,7 @@ let stockController = require("../controllers/stocks.controller");
 router.post("/store", stockController.storeStock); // Route to store stock
 router.get("/findAll", stockController.findAllStocks); // Route to find all stocks
 router.get("/searchStockBySymbol", stockController.searchBySymbol); // Route to seach one stock by symbol
-router.get("/findAllFromDb", stockController.findAllFromDb); 
+router.get("/findAllFromDb", stockController.findAllFromDb); // Route to seach all stocks stored in DB
 
 module.exports = {
     router
